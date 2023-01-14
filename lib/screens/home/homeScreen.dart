2d9724fe603dbc26ae40d0994dd8da29c31d4800
maramahmer <maramahmer@gmail.com>
2023-01-14 17:21:24 -0500

    new splash

--- lib/screens/home/homeScreen.dart
+++ lib/screens/home/homeScreen.dart
@@ -13,6 +13,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: bgColor,
