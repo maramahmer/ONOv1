6a15c5c4ddf7e2edc7091a1e8ea5de49e930d23d
maramahmer <35339061+maramahmer@users.noreply.github.com>
2023-01-13 21:55:50 -0500

    second page and home button
    TODO: implement bottom button to show all, and start creating route, create and replace buttons

--- lib/screens/secondarySelectionScreen.dart
+++ lib/screens/secondarySelectionScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ono/constants.dart';
+import 'package:ono/screens/home/homeScreen.dart';
 import '../../model/emotionButton.dart';
 
 class SecondarySelectionScreen extends StatefulWidget {
@@ -11,12 +12,31 @@ class SecondarySelectionScreen extends StatefulWidget {
   State<SecondarySelectionScreen> createState() =>
       _SecondarySelectionScreenState();
 }
-  class _SecondarySelectionScreenState extends State<SecondarySelectionScreen>{
+
+class _SecondarySelectionScreenState extends State<SecondarySelectionScreen> {
   int quantity = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: <Widget>[
+          Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                onTap: () {
+/*                   Navigator.pop(context, MaterialPageRoute(builder: (context) {
+                    return HomeScreen();
+                  })); */
+                  Navigator.of(context).pushAndRemoveUntil(
+                      MaterialPageRoute(builder: (context) => HomeScreen()),
+                      (Route<dynamic> route) => false);
+                },
+                child: const Icon(
+                  Icons.home,
+                  size: 30,
+                ),
+              )),
+        ],
         backgroundColor: bgColor,
         elevation: 0,
         iconTheme: const IconThemeData(
@@ -27,14 +47,22 @@ class SecondarySelectionScreen extends StatefulWidget {
       ),
       body: ListView(
         children: [
-          const SizedBox(height: 0,),
+          const SizedBox(),
+
           header(), // can you narrow that down
-          const SizedBox(height: 0,),
-          listEmotions(), // secondary emotion buttons (list format)
+
+          SizedBox(
+            height: 500,
+            child: //listEmotions(),
+                gridEmotionsSec(),
+          ),
+
+          showAll(), // show all ono in the broad category
         ],
       ),
     );
   }
+
   Widget header() {
     // widget implementation for appbar message "can you narrow that down?"
     return Padding(
@@ -44,65 +72,152 @@ class SecondarySelectionScreen extends StatefulWidget {
           Text(
             "can you narrow that down?",
             textAlign: TextAlign.center,
-            style: Theme
-                .of(context)
+            style: Theme.of(context)
                 .textTheme
-                .headlineSmall!
-                .copyWith(
-                fontWeight: FontWeight.w400,
-                color: mainPink),
+                .headlineMedium!
+                .copyWith(fontWeight: FontWeight.w400, color: mainPink),
           ),
-          Text(
-            widget.emotionButton.name,
-            style: Theme
-                .of(context)
-                .textTheme
-                .titleLarge!
-                .copyWith(fontWeight: FontWeight.w400,
-                color: mainBlue),
-          )
+          Padding(
+              padding: const EdgeInsets.all(18),
+              child: Text(
+                widget.emotionButton.name,
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(fontWeight: FontWeight.w400, color: mainBlue),
+              )),
         ],
       ),
     );
   }
 
-/*  Widget listEmotions(){
+  Widget listEmotions() {
     return SizedBox(
-      height: 50,
-      child: ListView.builder(
+      height: 1000,
+      width: 10,
+      child: ListView.separated(
         itemCount: widget.emotionButton.length,
-        itemBuilder: (context, index){
+        separatorBuilder: (BuildContext context, int index) {
+          return const Padding(
+            padding: EdgeInsets.all(defaultBorderRadius),
+          );
+        },
+        itemBuilder: (BuildContext context, int index) {
           return ButtonTheme(
             minWidth: 15.0,
-            height: 30.0,
+            height: 40.0,
             child: MaterialButton(
               color: mainBlue,
+              padding: const EdgeInsets.all(7),
+              minWidth: 15,
               colorBrightness: Brightness.dark,
-              onPressed: () =>(){},
-              shape: RoundedRectangleBorder(borderRadius:
-              BorderRadius.circular(10.0)),
-              child:  Text(widget.emotionButton.secondEmotionList[index]),
+              onPressed: () => () {},
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0)),
+              child: Text(
+                widget.emotionButton.secondEmotionList[index],
+                textAlign: TextAlign.center,
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineSmall!
+                    .copyWith(fontWeight: FontWeight.w300, color: Colors.white),
+              ),
             ),
           );
         },
       ),
     );
-  }*/
+  }
 
-  Widget listEmotions(){
-    return SizedBox(
-      height: 50,
-      child: ListView.builder(
-        itemCount: widget.emotionButton.length,
-        itemBuilder: (context, index){
-          return Padding(
-            padding: const EdgeInsets.all(defaultPadding),
-            child: Center(
-              child:  Text(widget.emotionButton.secondEmotionList[index]),
-            ),
-          );
-        },
+  Widget showAll() {
+    // widget implementation for appbar message "can you narrow that down?"
+    return Padding(
+      padding: const EdgeInsets.all(defaultPadding),
+      child: Column(
+        children: [
+          Text(
+            "show all",
+            textAlign: TextAlign.center,
+            style: Theme.of(context)
+                .textTheme
+                .headlineMedium!
+                .copyWith(fontWeight: FontWeight.w400, color: mainPink),
+          ),
+          Padding(
+              padding: const EdgeInsets.all(18),
+              child: Text(
+                widget.emotionButton.name,
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(fontWeight: FontWeight.w400, color: mainBlue),
+              )),
+        ],
       ),
+    );
+  }
+
+  Widget gridEmotionsSec() {
+    return GridView.builder(
+      itemCount: widget.emotionButton.secondEmotionList.length,
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      padding: const EdgeInsets.all(25),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 1,
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 10,
+        mainAxisExtent: 60,
+      ),
+      itemBuilder: (context, index) {
+        //EmotionButton emotionButton = emoButton[widget.emotionButton.name];
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              throw UnimplementedError("TODO SHOW ONO");
+            }));
+          },
+          child: Container(
+            height: 40,
+            decoration: BoxDecoration(
+              color:
+                  mainBlue, // CHANGE TO THIS ONCE REMAINDER OF BUTTON DONE TO MAKE TRANSPARENT
+              borderRadius: BorderRadius.circular(85),
+            ),
+            child: Stack(
+              children: [
+                Column(
+                  //crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Center(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                    const SizedBox(height: 0),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Text(
+                        widget.emotionButton.secondEmotionList[index],
+                        style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                        textAlign: TextAlign.center,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
