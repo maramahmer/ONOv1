56108e4818764fbbeb6874c9c2d903456110ebf4
maramahmer <35339061+maramahmer@users.noreply.github.com>
2023-02-02 10:14:39 -0500

    Create dummyListModel.dart

---
+++ lib/model/dummyListModel.dart
@@ -0,0 +1,24 @@
+class dummyListModel {
+  dummyListModel({
+    required this.number,
+    required this.letter,
+    required this.onomatopoeia,
+    required this.transliteration,
+    required this.frequency,
+    required this.meaningjp,
+    required this.meaningen,
+    required this.mainemotion,
+    required this.subemotion,
+    
+  });
+  int number;
+  String letter;
+  String onomatopoeia;
+  String transliteration;
+  String frequency;
+  String meaningjp;
+  String meaningen;
+  String mainemotion;
+  String subemotion;
+
+}
